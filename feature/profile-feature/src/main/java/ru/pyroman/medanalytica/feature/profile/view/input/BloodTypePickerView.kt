871421feafe736ddd1bf6f0d0d41f024c2415526
DoomPI <rslomtev@edu.hse.ru>
2024-03-24 22:49:15 +0500

    фикс бага с профилем

--- feature/profile-feature/src/main/java/ru/pyroman/medanalytica/feature/profile/view/input/BloodTypePickerView.kt
+++ feature/profile-feature/src/main/java/ru/pyroman/medanalytica/feature/profile/view/input/BloodTypePickerView.kt
@@ -1,7 +1,10 @@
 package ru.pyroman.medanalytica.feature.profile.view.input
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.DropdownMenuItem
 import androidx.compose.material.ExperimentalMaterialApi
 import androidx.compose.material.ExposedDropdownMenuBox
@@ -12,12 +15,15 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.TextUnit
 import androidx.compose.ui.unit.TextUnitType
 import androidx.compose.ui.unit.dp
+import ru.pyroman.medanalytica.base.uikit.R
 import ru.pyroman.medanalytica.domain.profile.domain.BloodType
 
 @OptIn(ExperimentalMaterialApi::class)
@@ -32,11 +38,15 @@ fun BloodTypePickerView(
     val bloodTypes = BloodType.entries.toTypedArray()
 
     ExposedDropdownMenuBox(
+        modifier = Modifier
+            .clip(RoundedCornerShape(4.dp))
+            .background(colorResource(id = R.color.lightGray))
+            .padding(4.dp),
         expanded = isExpanded,
         onExpandedChange = { isExpanded = it},
     ) {
         Text(
-            text = selectedBloodType?.value.orEmpty(),
+            text = selectedBloodType?.value ?: "Не выбрана",
             color = Color.Black,
             fontSize = TextUnit(14f, TextUnitType.Sp),
             fontWeight = FontWeight.Normal,
